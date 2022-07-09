0634e738a538fabc885eb6c95fbc17791bdc575b
imerla1 <giorgi1048@yahoo.com>
2022-07-09 17:31:15 +0400

    added logger to fastapi backend

--- fastapi_server.py
+++ fastapi_server.py
@@ -1,16 +1,25 @@
 from fastapi import Body, FastAPI, status
 from datetime import datetime, timedelta
 from typing import Union, Optional
-
-from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi import Depends, FastAPI, HTTPException, status, Request
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jose import JWTError, jwt
 from passlib.context import CryptContext
 from pydantic import BaseModel
+import logging
+import random, string, time
 
 app = FastAPI(
     title="FastAPI Oauth2 Implementation"
 )
+
+logging.basicConfig(filename="fastapi.log",
+                    filemode='a',
+                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                    datefmt='%H:%M:%S',
+                    level=logging.DEBUG)
+
+logger = logging.getLogger(__name__)
 
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
@@ -148,3 +157,17 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
 @app.get("/users/me/", response_model=UserBase)
 async def read_users_me(current_user: UserBase = Depends(get_current_active_user)):
     return current_user
+
+@app.middleware("http")
+async def log_requests(request: Request, call_next):
+    idem = ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
+    logger.info(f"rid={idem} start request path={request.url.path}")
+    start_time = time.time()
+    
+    response = await call_next(request)
+    
+    process_time = (time.time() - start_time) * 1000
+    formatted_process_time = '{0:.2f}'.format(process_time)
+    logger.info(f"rid={idem} completed_in={formatted_process_time}ms status_code={response.status_code}")
+    
+    return response
